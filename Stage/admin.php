ea16e075026cc8a73d155242cb7179071e78456b
Mazeen Aljane <68705861+ItzMazeen@users.noreply.github.com>
2021-07-16 18:18:15 +0100

    Add files via upload

---
+++ Stage/admin.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+
+	include("config.php");
+	include("functions.php");
+
+	$user_data = check_login($conn);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tt Sondage | Admin Page</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="admin.css">
+    <link rel="icon" type="image/png" href="logo.jpg" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+<body>
+<div style="text-align:center">
+	<a href="logout.php">Logout</a>
+    <br><br>
+	<h1>This is the ADMIN page</h1>
+
+	<br>
+	Hello, <?php echo $user_data['username']; ?>
+    </div>
+</body>
+</html>
